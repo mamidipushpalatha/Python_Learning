f25423789a047e25f6578edc546f7e4ec2c6a935
mamidipushpalatha <mamidilathareddy7@gmail.com>
2024-06-01 23:12:15 +0530

    Added Remove duplicates of sorrted Array program

---
+++ DSA/Arrays/REmoveDuplicatesfromSortedArray.py
@@ -0,0 +1,11 @@
+class Solutions:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
+
+
+print(Solutions().removeDuplicates([0, 0, 1, 1, 2]))
